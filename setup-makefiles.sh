afd3a88e10252fd1d7b2f27c0e40bd999635d6ac
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-06-15 21:13:23 +0530

    sdm845-common: Kronic-ify setup-makefile and extract-files
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I60fcf4f6e74c80dd8f4fc178262e01a4ea5b56e5

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,20 +21,20 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+AOSIP_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${AOSIP_ROOT}/vendor/aosip/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${AOSIP_ROOT}" true
 
 # Copyright headers and guards
-write_headers "enchilada"
+write_headers "enchilada fajita"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt true
@@ -44,7 +44,8 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false
+    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${AOSIP_ROOT}" false
 
     # Copyright headers and guards
     write_headers
