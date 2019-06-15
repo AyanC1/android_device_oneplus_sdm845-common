bcf99bc13b1757e56ce0432d6a7dab3fd0ff2b60
LuK1337 <priv.luk@gmail.com>
2019-06-15 15:40:37 +0200

    sdm845-common: Update ssr restart level prop
    Change-Id: Icaca77128671f1b0180cc3315f062f3f123f8b04
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -93,7 +93,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.ssr.restart_level=ALL_ENABLE
+    persist.sys.ssr.restart_level=ALL_ENABLE \
+    persist.vendor.ssr.restart_level=ALL_ENABLE
 
 #Google Assistant enabled here
 PRODUCT_PROPERTY_OVERRIDES += \
