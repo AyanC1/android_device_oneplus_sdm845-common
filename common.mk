18430251c9a8724fba018d0c728fe276aae09afc
AyanC1 <choudhury.ayan13@gmail.com>
2020-08-28 12:38:35 +0530

    sdm845-common: "add Gcam"

--- common.mk
+++ common.mk
@@ -84,7 +84,11 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    GoogleCamera \
+    ARCore
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.xml
 
 # Common init scripts
 PRODUCT_PACKAGES += \
