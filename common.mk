d5bc952fb09be4a9fbe8aebacfccb856da34fb17
AyanC1 <choudhury.ayan13@gmail.com>
2020-10-09 20:09:57 +0530

    telephony changes

--- common.mk
+++ common.mk
@@ -172,8 +172,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+ #   telephony-ext
 
 # tri-state-key
 PRODUCT_PACKAGES += \
