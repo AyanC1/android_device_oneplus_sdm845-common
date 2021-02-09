ac2d0b01bfa462255168faf2fed33bbf3ac11dd0
AyanC1 <choudhury.ayan13@gmail.com>
2021-02-09 23:41:37 +0530

    Devicesettings: fix paths

--- DeviceSettings/src/com/potato/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/com/potato/device/DeviceSettings/DeviceSettings.java
@@ -39,7 +39,7 @@ import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.TwoStatePreference;
 
-import com.android.internal.util.arrow.FileUtils;
+import com.android.internal.util.dot.FileUtils;
 
 public class DeviceSettings extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener {
